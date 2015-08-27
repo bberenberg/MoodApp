53742c909d5722b8dbcbf30e84103aa332fcb286
Boris Berenberg <bberenberg@users.noreply.github.com>
2015-08-27 14:38:02 -0700

    found ux notes again

---
+++ src/UXNotes.js
@@ -0,0 +1,31 @@
+Bar graph not obvious to enter
+Up and down also not obvious
+Reminders phrase not clear, communicate that it is mins better
+Why reminders
+Grey subtext is non obvious
+Reminders vs alerts phrasing
+Avg is for me or others?
+Why sum vs avg
+What is the middle button for
+Reminders is count of reminders?
+Random reminders
+More time options
+More detail on the graph
+Reminders->
+- Hey, gratz on being happy
+Asks you a question or prompt
+- Shallow or deep breath?
+- body awareness - body scanning
+Heart rate
+DBT - dialectic behavioral therapy
+Am I around people?
+Calendar of appointments
+Interface with google cal, overlay ratings with events
+More prompts for positive thoughts 
+Up and down are settings in the main thing, not data points
+Mood ring? Heartbeat based emotions device
+Not enough information
+Support or help with goals
+- Today was a good day
+Guide to anywhere
+- small silly tasks
